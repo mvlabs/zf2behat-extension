9ef96eb326b989656bf8ee2bf0797b86f693b2b0
Mauipipe <mauilap@mauilap-Think.(none)>
2013-08-04 19:25:49 +0200

    zf2app simplified

--- testapp/zf2mini/module/ModuleExample/config/module.config.php
+++ testapp/zf2mini/module/ModuleExample/config/module.config.php
@@ -25,35 +25,7 @@ return array(
                             ),
                         ),
                     ),
-                    
-                     'asset_manager' => array(
-        'resolver_configs' => array(
-    
-            'paths' => array(
-                __DIR__ . '/../assets',
-            ),
-            
-        ),
-    ),
-    
-     'doctrine' => array(
-        'driver' => array(
-            __NAMESPACE__ . '_driver' => array(
-                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
-                'cache' => 'array',
-                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
-            ),
-            'orm_default' => array(
-                'drivers' => array(
-                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
-                )
-            ),
-        ),
-    ),
-      
-	'view_manager'=>array(
-		'template_path_stack'=>array(
-			'albums'=>__DIR__.'/../view',
-		),
-	),
+        
+           
+	
 );
