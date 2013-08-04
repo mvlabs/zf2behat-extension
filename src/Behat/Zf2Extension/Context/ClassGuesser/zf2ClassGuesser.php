62ae6ecde9ea019f6997eb78f29575d2f3c9abc6
Mauipipe <mauilap@mauilap-Think.(none)>
2013-08-04 15:44:56 +0200

    class guesser created

---
+++ src/Behat/Zf2Extension/Context/ClassGuesser/zf2ClassGuesser.php
@@ -0,0 +1,25 @@
+<?php
+namespace Behat\Zf2Extension\Context\ClassGuesser;
+
+use Behat\Behat\Context\ClassGuesser\ClassGuesserInterface;
+
+/**
+ * Description of zf2ClassGuesser
+ *
+ * @author David Contavalli < mauipipe@gmail.com >
+ * @copyright M.V. Labs (c) 2011 - All Rights Reserved -
+ *  You may execute and modify the contents of this file, but only within the scope of this project.
+ *  Any other use shall be considered forbidden, unless otherwise specified.
+ * @link http://www.mvassociates.it
+ */
+class zf2ClassGuesser implements ClassGuesserInterface{
+   
+    public function guess() {
+    
+        return "Behat\Zf2Extension\Context\Zf2ApplicationContext";
+        
+    }   
+    
+}
+
+?>
